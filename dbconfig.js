566354d4a0d5862ae233cb7316204af0efb2e5c8
Alina Saghir <alinasaghir97@gmail.com>
2025-06-28 14:10:47 +0500

    Prepare for rebase: local changes

--- dbconfig.js
+++ dbconfig.js
@@ -1,3 +1,2 @@
 module.exports = {
-  connectionString: "Driver={ODBC Driver 17 for SQL Server};Server=admin.cn4gwgsm65h0.eu-north-1.rds.amazonaws.com,1433;Database=medoQRST;Uid=admin;Pwd=alina121;"
-}
+  connectionString: "enter your connection string"}
